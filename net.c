2b55d67048672fe774fa1d84c1e0c0c30487080e
kwkt <44708388+kwQt@users.noreply.github.com>
2021-05-03 20:58:58 +0900

    Finish step6

--- net.c
+++ net.c
@@ -95,11 +95,31 @@ net_device_close(struct net_device *dev)
 int
 net_device_add_iface(struct net_device *dev, struct net_iface *iface)
 {
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next) {
+        if (entry->family == iface->family) {
+            errorf("already exists, dev=%s, family=%d", dev->name, entry->family);
+            return -1;
+        }
+    }
+    iface->dev = dev;
+    iface->next = dev->ifaces;
+    dev->ifaces = iface;
+    return 0;
 }
 
 struct net_iface *
 net_device_get_iface(struct net_device *dev, int family)
 {
+    struct net_iface *entry;
+
+    for (entry = dev->ifaces; entry; entry = entry->next) {
+        if (entry->family == family) {
+            return entry;
+        }
+    }
+    return NULL;
 }
 
 int
