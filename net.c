ef4f026f9cec62539dac65690e41fe3a0d56c99a
kwkt <44708388+kwQt@users.noreply.github.com>
2021-06-06 15:36:47 +0900

    Finish step19

--- net.c
+++ net.c
@@ -12,6 +12,7 @@
 #include "arp.h"
 #include "icmp.h"
 #include "ip.h"
+#include "tcp.h"
 #include "udp.h"
 #include "util.h"
 
@@ -423,6 +424,10 @@ net_init(void)
     }
     if (udp_init() == -1) {
         errorf("udp_init() failure");
+        return -1;
+    }
+    if (tcp_init() == -1) {
+        errorf("tcp_init() failure");
         return -1;
     }
     return 0;
